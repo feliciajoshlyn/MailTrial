bd16664763d8485a25b6835848297022807650f8
Feli <feliciajoshlyn@gmail.com>
2025-11-05 10:00:43 +0700

    Add favicon to welcome page
    Added cloud.png to the public directory and updated welcome.blade.php to use it as the favicon for the application.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Welcome to Cloud Class ☁️</title>
+
+  <link rel="icon" type="image/png" href="{{ asset('cloud.png') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
