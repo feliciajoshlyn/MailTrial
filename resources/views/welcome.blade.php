472e88953662ab89da892f935d4f35c67069dc02
Feli <feliciajoshlyn@gmail.com>
2025-11-05 10:01:41 +0700

    Update page title in welcome view
    Shortened the HTML title from 'Welcome to Cloud Class ☁️' to 'Cloud Class ☁️' in the welcome.blade.php view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Welcome to Cloud Class ☁️</title>
+  <title>Cloud Class ☁️</title>
 
   <link rel="icon" type="image/png" href="{{ asset('cloud.png') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
